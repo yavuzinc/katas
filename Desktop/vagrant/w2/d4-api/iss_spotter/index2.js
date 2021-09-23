b4ef8ac00ec88d54ce797a90b46dffaa743c8ac2
Yavuz Incedal <yavuzincedalca@gmail.com>
2021-09-23 16:46:11 -0400

    promised applied

---
+++ Desktop/vagrant/w2/d4-api/iss_spotter/index2.js
@@ -0,0 +1,13 @@
+// index2.js
+const { nextISSTimesForMyLocation, printPassTimes } = require('./iss_promised');
+
+// see index.js for printPassTimes
+// copy it from there, or better yet, moduralize and require it in both files
+
+// Call
+nextISSTimesForMyLocation()
+  .then((passTimes) => {
+    printPassTimes(passTimes);
+  }).catch((error) => {
+    console.log("It didn't work: ", error.message);
+  });
