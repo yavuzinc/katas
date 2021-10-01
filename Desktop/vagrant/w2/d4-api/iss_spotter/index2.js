1914b8b74a33c573a9ceaa5ea6a52e34a2e1a485
Yavuz <88813478+yavuzinc@users.noreply.github.com>
2021-09-30 21:26:23 -0400

    Delete Desktop/vagrant directory

--- Desktop/vagrant/w2/d4-api/iss_spotter/index2.js
+++
@@ -1,8 +0,0 @@
-const { nextISSTimesForMyLocation, printPassTimes } = require("./iss_promised");
-nextISSTimesForMyLocation()
-  .then((passTimes) => {
-    printPassTimes(passTimes);
-  })
-  .catch((error) => {
-    console.log("It didn't work: ", error.message);
-  });
